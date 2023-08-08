7bd19fb75a1c6baf48d065b67339c41694120d2d
ddragnell <andressalbecerra@gmail.com>
2023-08-08 18:04:40 -0500

    Feat: Carrito

--- Proyecto/front-end/src/AppRoutes.js
+++ Proyecto/front-end/src/AppRoutes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './Pages/Home/HomePage'
 import FoodPage from './Pages/Food/FoodPage';
+import CartPage from './Pages/Cart/CartPage';
 
 export default function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function AppRoutes() {
         <Route path="/buscar/:searchTerm" element={<HomePage />} />
         <Route path="/etiqueta/:tag" element={<HomePage />} />
         <Route path="/comidas/:id" element={<FoodPage />} />
+        <Route path="/carrito" element={<CartPage />} />
     </Routes>
   );
 
---
+++ Proyecto/front-end/src/Pages/Cart/CartPage.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import classes from './cartPage.module.css'
+import { useCart } from '../../hooks/useCart'
+import Title from '../../Components/Title/Title';
+import { Link } from 'react-router-dom';
+import Price from '../../Components/Price/Price';
+
+export default function CartPage() {
+  
+    const { cart } = useCart();
+
+    return (
+        <>
+            <Title title = "Carrito de compras" margin = "1.5rem 0 0 2.5rem" />
+
+            {cart && cart.items.length > 0 && 
+
+                <div className={classes.container}>
+                    <ul className={classes.list}>
+                        {cart.items.map(item => 
+                            <li key={item.food.id}>
+                                <div>
+                                    <img 
+                                        src = {`/foods/${item.food.imageUrl}`} 
+                                        alt = {item.food.name}
+                                    />
+                                </div>
+                                <div>
+                                    <Link to={`/comidas/${item.food.id}`}>{item.food.name}</Link>
+                                </div>
+
+                                <div>
+                                    <select value = {item.quantity}>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        <option>8</option>
+                                        <option>9</option>
+                                        <option>10</option>
+                                    </select>
+                                </div>
+
+                                <div>
+                                    <Price price={item.price} />
+                                </div>
+
+                                <div>
+                                    <button className={classes.remove_button}>
+                                        Remover producto
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+                    </ul>
+
+
+                    <div className={classes.checkout}>
+                        <div>
+                            <div className={classes.foods_count}>{cart.totalCount}</div>
+                            <div className={classes.total_price}>
+                                <Price price={cart.totalPrice} />
+                            </div>
+                        </div>
+
+                        <Link to="/pago">Proceder al pago</Link>
+                    </div>
+                </div>
+ 
+            }
+        </>
+    );
+}
